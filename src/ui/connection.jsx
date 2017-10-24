1902fb2a8feb9a1bf01bac336427b44610420508
Andrés Maneiro <andres.maneiro@automattic.com>
2017-10-24 20:17:29 +0200

    Use Connection component from Calypso

--- src/ui/connection.jsx
+++ src/ui/connection.jsx
@@ -1,35 +1,24 @@
+/** @format */
+
 /**
  * External dependencies
  */
-import { Component } from 'react';
 import { connect } from 'react-redux';
-import debugFactory from 'debug';
+
+/**
+ * Calypso dependencies
+ */
+import { HappychatConnection } from 'components/happychat/connection';
+
 /**
  * Internal dependencies
  */
-import config from 'src/config';
 import { connectChat } from 'src/state/socket/actions';
 import { isHappychatConnectionUninitialized } from 'src/state/socket/selectors';
 
-const debug = debugFactory( 'happychat-embedded:ui:connection' );
-
-class HappychatConnection extends Component {
-	componentDidMount() {
-		debug( 'component did mount' );
-		if ( config.isEnabled( 'happychat' ) && this.props.isUninitialized ) {
-			debug( 'dispatch connect action' );
-			this.props.connectChat();
-		}
-	}
-
-	render() {
-		return null;
-	}
-}
-
 export default connect(
 	state => ( {
-		isUninitialized: isHappychatConnectionUninitialized( state )
+		isUninitialized: isHappychatConnectionUninitialized( state ),
 	} ),
 	{ connectChat }
 )( HappychatConnection );
