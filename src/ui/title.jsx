9c4586108e53f7de6374e5730bad5430f14c8d6f
Andrés Maneiro <andres.maneiro@automattic.com>
2017-11-01 17:48:30 +0100

    Title uses calypso pure component

--- src/ui/title.jsx
+++ src/ui/title.jsx
@@ -4,7 +4,19 @@
  * External dependencies
  */
 import { Title } from 'components/happychat/title';
+import { connect } from 'react-redux';
+
+/**
+ * Calypso dependencies
+ */
 // TODO: implement localize
 import { mockLocalize } from 'src/ui/components/localize';
 
-export default mockLocalize( Title );
+export default connect(
+	{},
+	{
+		onCloseChat: () => {},
+		onMinimizeChat: () => {},
+		onMinimizedChat: () => {},
+	}
+)( mockLocalize( Title ) );
