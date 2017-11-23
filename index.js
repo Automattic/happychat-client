40f58265370fb30149dbb9248725f00e065a544f
Andrés Maneiro <andres.maneiro@automattic.com>
2017-11-23 18:15:22 +0100

    Pack import/export

--- index.js
+++ index.js
@@ -4,13 +4,12 @@
  * Internal dependencies
  */
 // UI components
-import { HappychatConnection } from './src/ui/components/connection';
-import { Composer } from './src/ui/components/composer';
-import { Notices } from './src/ui/components/notices';
-import { Timeline } from './src/ui/components/timeline';
-import { Title } from './src/ui/components/title';
+export { HappychatConnection } from './src/ui/components/connection';
+export { Composer } from './src/ui/components/composer';
+export { Notices } from './src/ui/components/notices';
+export { Timeline } from './src/ui/components/timeline';
+export { Title } from './src/ui/components/title';
 
 // State components
 import reducer from './src/state/reducer';
-
-export { reducer, HappychatConnection, Composer, Notices, Timeline, Title };
+export { reducer };
