6b7fade77152c786f13af4bc62b77d3264f1e422
Andrés Maneiro <andres.maneiro@automattic.com>
2017-12-20 17:00:49 +0100

    Add user_eligibility conditions
    `paying_customer` logic is copied from the current chat plugin used in
    WooCommerce.com: wccom-olark/client/class-wccom-olark-client.php

--- targets/wordpress/plugin/class-happychat-client.php
+++ targets/wordpress/plugin/class-happychat-client.php
@@ -42,14 +42,91 @@ class Happychat_Client {
 				</span>';
 	}
 
+	/**
+	 * Returns whether Happychat is enabled or not.
+	 *
+	 * @return bool
+	 */
 	private function is_happychat_enabled() {
 		return get_option( 'happychat_enable' );
 	}
 
-	private function is_paying_customer() {
+	/**
+	 * Get all product IDs that have attribute "Revenue Share" = true.
+	 *
+	 * @return array
+	 */
+	public function get_revenue_share_products() {
+		$result = array();
+		$page = 0;
+		$products_per_page = 500;
+		while ( $product_ids = get_posts(
+			array(
+				'posts_per_page' => $products_per_page,
+				'offset' => $products_per_page * $page++,
+				'fields' => 'ids',
+				'post_type' => 'product',
+				'tax_query' => array(
+					array(
+						'taxonomy' => 'pa_revenue-share',
+						'field' => 'slug',
+						'terms' => 'true',
+					),
+				),
+			)
+		) ) {
+			$result = array_merge( $result, $product_ids );
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Returns true if customer has a current license for a non-free product.
+	 * If the ticket creation page is loading slowly for customers with many
+	 * products while chat blitz is on, this is a likely culprit.
+	 *
+	 * @param $user_id
+	 * @return bool
+	 */
+	public function is_paying_customer() {
+		global $wpdb;
+		$user_id = get_current_user_id();
+
+		// Look up current licenses.
+		$statement = $wpdb->prepare(
+			"SELECT product_id
+			FROM {$wpdb->prefix}woocommerce_product_keys
+			WHERE %d = user_id
+			AND ( 1 = lifetime
+				OR support_expiry_date > NOW() )",
+			$user_id
+		);
+		$product_ids = $wpdb->get_col( $statement, 0 );
+
+		// Check if any of the licensed products are revenue share products.
+		if ( ! empty( array_intersect( $this->get_revenue_share_products(), $product_ids ) ) ) {
+			// Customer has a revenue share product.
+			return true;
+		}
+
+		// Check if any of the licensed products cost money.
+		foreach ( $product_ids as $product_id ) {
+			$product = wc_get_product( $product_id );
+			if ( 0 < $product->get_regular_price() ) {
+				// User has a paid product.
+				return true;
+			}
+		}
+
 		return false;
 	}
 
+	/**
+	 * Returns true if the user is eligible to show chat support.
+	 *
+	 * @return bool
+	 */
 	private function is_user_eligible() {
 		$is_eligible = false;
 		$eligibility = get_option( 'happychat_user_eligibility' );
@@ -66,6 +143,11 @@ class Happychat_Client {
 		return $is_eligible;
 	}
 
+	/**
+	 * Returns true if chat should be offered.
+	 *
+	 * @return bool
+	 */
 	private function should_offer_chat() {
 		global $wp;
 		$should_offer_chat = false;
