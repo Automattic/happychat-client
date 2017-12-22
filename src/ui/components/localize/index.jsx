96550b7fc70722e53e635f004e741909e347a933
Andrés Maneiro <andres.maneiro@automattic.com>
2017-12-22 11:28:34 +0100

    Implement options.components for translate mock

--- src/ui/components/localize/index.jsx
+++ src/ui/components/localize/index.jsx
@@ -5,6 +5,7 @@
  */
 import React, { Component } from 'react';
 import get from 'lodash/get';
+import interpolateComponents from 'interpolate-components';
 
 export const mockLocalize = ComposedComponent =>
 	class extends Component {
@@ -12,6 +13,12 @@ export const mockLocalize = ComposedComponent =>
 			let translatedMsg = msg;
 			if ( get( options, 'args', false ) ) {
 				translatedMsg = msg.replace( '%s', options.args );
+			}
+			if ( get( options, 'components', false ) ) {
+				translatedMsg = interpolateComponents( {
+					mixedString: translatedMsg,
+					components: options.components,
+				} );
 			}
 			return translatedMsg;
 		}
