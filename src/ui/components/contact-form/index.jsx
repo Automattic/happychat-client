e63b9b0cc5b1d7393353bb16e009eb8bfcbc6f99
Andrés Maneiro <andres.maneiro@automattic.com>
2017-12-28 12:16:55 +0100

    Add segmented-control

--- src/ui/components/contact-form/index.jsx
+++ src/ui/components/contact-form/index.jsx
@@ -13,31 +13,46 @@ const debug = debugFactory( 'happychat-client:ui:contact-form' );
 import FormTextarea from 'src/ui/components/form-textarea';
 import FormLabel from 'src/ui/components/form-label';
 import FormButton from 'src/ui/components/form-button';
+import SegmentedControl from 'src/ui/components/segmented-control';
 
 export class ContactForm extends React.Component {
-	submitForm() {
-		debug( 'submit form' );
+	constructor( props ) {
+		super( props );
+		this.state = { message: '' };
+		this.handleChange = this.handleChange.bind( this );
 	}
 
-	handleChange() {
-		debug( 'handle change' );
+	handleChange( e ) {
+		debug( 'setState to ', e.target.value );
+		this.setState( { message: e.target.value } );
 	}
 
 	canSubmitForm() {
-		debug( 'canSubmitForm' );
-		return true;
+		debug( 'canSubmitForm ', this.state.message !== '' );
+		return this.state.message !== '';
 	}
 
 	render() {
+		const { submitForm } = this.props;
+
+		const options = [
+			{ value: 'account', label: 'Account', subtext: 'Change pwd, etc' },
+			{ value: 'extensions', label: 'Extensions', subtext: 'Primary, etc' },
+		];
 		return (
 			<div className="help-contact-form">
+				<FormLabel>How can we help?</FormLabel>
+				<SegmentedControl options={ options } primary />
+
 				<FormLabel>What are you trying to do?</FormLabel>
 				<FormTextarea
 					placeholder="Please be descriptive"
 					name="message"
+					value={ this.state.message }
 					onChange={ this.handleChange }
 				/>
-				<FormButton disabled={ ! this.canSubmitForm() } type="button" onClick={ this.submitForm }>
+
+				<FormButton disabled={ ! this.canSubmitForm() } type="button" onClick={ submitForm }>
 					Send
 				</FormButton>
 			</div>
