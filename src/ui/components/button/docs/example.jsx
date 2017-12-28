6a667fd49c502332f0829b09279119d4ba170e1f
Andrés Maneiro <andres.maneiro@automattic.com>
2017-12-28 11:47:00 +0100

    Some progress

---
+++ src/ui/components/button/docs/example.jsx
@@ -0,0 +1,270 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+
+import React from 'react';
+import Gridicon from 'gridicons';
+
+/**
+ * Internal dependencies
+ */
+import Button from 'components/button';
+import Card from 'components/card';
+import config from 'config';
+import DocsExample from 'devdocs/docs-example';
+
+class Buttons extends React.PureComponent {
+	static displayName = 'Buttons';
+
+	state = {
+		compactButtons: false,
+	};
+
+	render() {
+		var toggleText = this.state.compactButtons ? 'Normal Buttons' : 'Compact Buttons';
+		return config.isEnabled( 'devdocs/components-usage-stats' )
+			? this.renderDocsExampleWithUsageStats( toggleText )
+			: this.renderDocsExample( toggleText );
+	}
+
+	renderDocsExample = toggleText => {
+		return (
+			<div>
+				<a className="docs__design-toggle button" onClick={ this.toggleButtons }>
+					{ toggleText }
+				</a>
+				{ this.renderButtons( toggleText ) }
+			</div>
+		);
+	};
+
+	renderDocsExampleWithUsageStats = toggleText => {
+		return (
+			<DocsExample
+				componentUsageStats={ this.props.componentUsageStats }
+				toggleHandler={ this.toggleButtons }
+				toggleText={ toggleText }
+			>
+				{ this.renderButtons() }
+			</DocsExample>
+		);
+	};
+
+	renderButtons = () => {
+		if ( ! this.state.compactButtons ) {
+			return (
+				<Card>
+					<div className="docs__design-button-row">
+						<Button>Button</Button>
+						<Button>
+							<Gridicon icon="heart" /> Icon button
+						</Button>
+						<Button>
+							<Gridicon icon="plugins" />
+						</Button>
+						<Button disabled>Disabled button</Button>
+					</div>
+					<div className="docs__design-button-row">
+						<Button scary>Scary button</Button>
+						<Button scary>
+							<Gridicon icon="globe" /> Scary icon button
+						</Button>
+						<Button scary>
+							<Gridicon icon="pencil" />
+						</Button>
+						<Button scary disabled>
+							Scary disabled button
+						</Button>
+					</div>
+					<div className="docs__design-button-row">
+						<Button primary>Primary button</Button>
+						<Button primary>
+							<Gridicon icon="camera" /> Primary icon button
+						</Button>
+						<Button primary>
+							<Gridicon icon="time" />
+						</Button>
+						<Button primary disabled>
+							Primary disabled button
+						</Button>
+					</div>
+					<div className="docs__design-button-row">
+						<Button primary scary>
+							Primary scary button
+						</Button>
+						<Button primary scary>
+							<Gridicon icon="user-circle" /> Primary scary icon button
+						</Button>
+						<Button primary scary>
+							<Gridicon icon="cart" />
+						</Button>
+						<Button primary scary disabled>
+							Primary scary disabled button
+						</Button>
+					</div>
+					<div className="docs__design-button-row">
+						<Button borderless>
+							<Gridicon icon="cross" /> Remove
+						</Button>
+						<Button borderless>
+							<Gridicon icon="trash" /> Trash
+						</Button>
+						<Button borderless>
+							<Gridicon icon="link-break" /> Disconnect
+						</Button>
+						<Button borderless>
+							<Gridicon icon="trash" />
+						</Button>
+						<Button borderless disabled>
+							<Gridicon icon="cross" /> Remove
+						</Button>
+					</div>
+					<div className="docs__design-button-row">
+						<Button borderless scary>
+							<Gridicon icon="cross" /> Remove
+						</Button>
+						<Button borderless scary>
+							<Gridicon icon="trash" /> Trash
+						</Button>
+						<Button borderless scary>
+							<Gridicon icon="link-break" /> Disconnect
+						</Button>
+						<Button borderless scary>
+							<Gridicon icon="trash" />
+						</Button>
+						<Button borderless scary disabled>
+							<Gridicon icon="cross" /> Remove
+						</Button>
+					</div>
+					<div className="docs__design-button-row">
+						<Button busy>Busy button</Button>
+						<Button primary busy>
+							<Gridicon icon="camera" /> Primary icon button
+						</Button>
+						<Button primary busy>
+							<Gridicon icon="time" />
+						</Button>
+						<Button primary busy>
+							Primary busy button
+						</Button>
+					</div>
+				</Card>
+			);
+		} else {
+			return (
+				<Card>
+					<div className="docs__design-button-row">
+						<Button compact>Compact button</Button>
+						<Button compact>
+							<Gridicon icon="heart" /> Compact icon button
+						</Button>
+						<Button compact>
+							<Gridicon icon="plugins" />
+						</Button>
+						<Button compact disabled>
+							Compact disabled button
+						</Button>
+					</div>
+					<div className="docs__design-button-row">
+						<Button compact scary>
+							Compact scary button
+						</Button>
+						<Button compact scary>
+							<Gridicon icon="globe" /> Compact scary icon button
+						</Button>
+						<Button compact scary>
+							<Gridicon icon="pencil" />
+						</Button>
+						<Button compact scary disabled>
+							Compact scary disabled button
+						</Button>
+					</div>
+					<div className="docs__design-button-row">
+						<Button compact primary>
+							Compact primary button
+						</Button>
+						<Button compact primary>
+							<Gridicon icon="camera" /> Compact primary icon button
+						</Button>
+						<Button compact primary>
+							<Gridicon icon="time" />
+						</Button>
+						<Button compact primary disabled>
+							Compact primary disabled button
+						</Button>
+					</div>
+					<div className="docs__design-button-row">
+						<Button compact primary scary>
+							Compact primary scary button
+						</Button>
+						<Button compact primary scary>
+							<Gridicon icon="user-circle" /> Compact primary scary icon button
+						</Button>
+						<Button compact primary scary>
+							<Gridicon icon="cart" />
+						</Button>
+						<Button compact primary scary disabled>
+							Compact primary scary disabled button
+						</Button>
+					</div>
+					<div className="docs__design-button-row">
+						<Button compact borderless>
+							<Gridicon icon="cross" /> Remove
+						</Button>
+						<Button compact borderless>
+							<Gridicon icon="trash" /> Trash
+						</Button>
+						<Button compact borderless>
+							<Gridicon icon="link-break" /> Disconnect
+						</Button>
+						<Button compact borderless>
+							<Gridicon icon="trash" />
+						</Button>
+						<Button compact borderless disabled>
+							<Gridicon icon="cross" /> Remove
+						</Button>
+					</div>
+					<div className="docs__design-button-row">
+						<Button compact borderless scary>
+							<Gridicon icon="cross" /> Remove
+						</Button>
+						<Button compact borderless scary>
+							<Gridicon icon="trash" /> Trash
+						</Button>
+						<Button compact borderless scary>
+							<Gridicon icon="link-break" /> Disconnect
+						</Button>
+						<Button compact borderless scary>
+							<Gridicon icon="trash" />
+						</Button>
+						<Button compact borderless scary disabled>
+							<Gridicon icon="cross" /> Remove
+						</Button>
+					</div>
+					<div className="docs__design-button-row">
+						<Button compact busy>
+							Busy button
+						</Button>
+						<Button compact primary busy>
+							<Gridicon icon="camera" /> Primary icon button
+						</Button>
+						<Button compact primary busy>
+							<Gridicon icon="time" />
+						</Button>
+						<Button compact primary busy>
+							Primary busy button
+						</Button>
+					</div>
+				</Card>
+			);
+		}
+	};
+
+	toggleButtons = () => {
+		this.setState( { compactButtons: ! this.state.compactButtons } );
+	};
+}
+
+export default Buttons;
--- src/ui/components/contact-form/index.jsx
+++ src/ui/components/contact-form/index.jsx
@@ -7,16 +7,39 @@ import React from 'react';
 import debugFactory from 'debug';
 const debug = debugFactory( 'happychat-client:ui:contact-form' );
 
+/**
+ * Internal dependencies
+ */
+import FormTextarea from 'src/ui/components/form-textarea';
+import FormLabel from 'src/ui/components/form-label';
+import FormButton from 'src/ui/components/form-button';
+
 export class ContactForm extends React.Component {
 	submitForm() {
 		debug( 'submit form' );
 	}
 
+	handleChange() {
+		debug( 'handle change' );
+	}
+
+	canSubmitForm() {
+		debug( 'canSubmitForm' );
+		return true;
+	}
+
 	render() {
 		return (
 			<div className="help-contact-form">
-				<textarea placeholder="Please, be descriptive" />
-				<button onClick={ this.submitForm } />
+				<FormLabel>What are you trying to do?</FormLabel>
+				<FormTextarea
+					placeholder="Please be descriptive"
+					name="message"
+					onChange={ this.handleChange }
+				/>
+				<FormButton disabled={ ! this.canSubmitForm() } type="button" onClick={ this.submitForm }>
+					Send
+				</FormButton>
 			</div>
 		);
 	}
