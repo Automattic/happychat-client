f0cb5d8c7882984272f29a669fa82d003f40e9ee
Andrés Maneiro <andres.maneiro@automattic.com>
2017-12-29 12:24:42 +0100

    Create HappychatForm
    To maximize reuse, this is created as a:
    
    - unconnected component
    - unaware of the happychat connection

---
+++ src/ui/components/happychat-form/index.js
@@ -0,0 +1,109 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+/**
+ * Internal dependencies
+ */
+import { Composer } from 'src/ui/components/composer';
+import { Notices } from 'src/ui/components/notices';
+import { Timeline } from 'src/ui/components/timeline';
+
+/**
+ * React component for rendering a happychat client
+ */
+export class HappychatForm extends Component {
+	constructor( props ) {
+		super( props );
+		this.onUnload = this.onUnload.bind( this );
+	}
+
+	componentDidMount() {
+		this.props.setFocused();
+		window.addEventListener( 'beforeunload', this.onUnload );
+	}
+
+	componentWillUnmount() {
+		this.props.setBlurred();
+		window.removeEventListener( 'beforeunload', this.onUnload );
+	}
+
+	onUnload( e ) {
+		e.returnValue = 'The chat session will end if the page is reloaded';
+		return e.returnValue;
+	}
+
+	render() {
+		const {
+			chatStatus,
+			connectionStatus,
+			currentUserEmail,
+			disabled,
+			isCurrentUser,
+			isExternalUrl,
+			isServerReachable,
+			message,
+			onSendMessage,
+			onSendNotTyping,
+			onSendTyping,
+			onSetCurrentMessage,
+			timeline,
+			translate,
+			twemojiUrl,
+		} = this.props;
+
+		return (
+			<div className="happychat__page" aria-live="polite" aria-relevant="additions">
+				<Timeline
+					currentUserEmail={ currentUserEmail }
+					isCurrentUser={ isCurrentUser }
+					isExternalUrl={ isExternalUrl }
+					timeline={ timeline }
+					translate={ translate }
+					twemojiUrl={ twemojiUrl }
+				/>
+				<Notices
+					chatStatus={ chatStatus }
+					connectionStatus={ connectionStatus }
+					isServerReachable={ isServerReachable }
+					translate={ translate }
+				/>
+				<Composer
+					disabled={ disabled }
+					message={ message }
+					onSendMessage={ onSendMessage }
+					onSendNotTyping={ onSendNotTyping }
+					onSendTyping={ onSendTyping }
+					onSetCurrentMessage={ onSetCurrentMessage }
+					translate={ translate }
+				/>
+			</div>
+		);
+	}
+}
+
+HappychatForm.propTypes = {
+	chatStatus: PropTypes.string,
+	connectionStatus: PropTypes.string,
+	currentUserEmail: PropTypes.string,
+	disabled: PropTypes.bool,
+	isCurrentUser: PropTypes.func,
+	isExternalUrl: PropTypes.func,
+	isServerReachable: PropTypes.bool,
+	message: PropTypes.string,
+	onSendMessage: PropTypes.func,
+	onSendNotTyping: PropTypes.func,
+	onSendTyping: PropTypes.func,
+	onSetCurrentMessage: PropTypes.func,
+	setBlurred: PropTypes.func,
+	setFocused: PropTypes.func,
+	timeline: PropTypes.array,
+	translate: PropTypes.func,
+	twemojiUrl: PropTypes.string,
+};
+
+export default HappychatForm;
