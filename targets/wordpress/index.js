5d7bcda9b0d551fb48a60949d6f0da3f815ed359
Andrés Maneiro <andres.maneiro@automattic.com>
2018-01-04 17:15:42 +0100

    Bring back subscribeTo, unsubscribeForm

--- targets/wordpress/index.js
+++ targets/wordpress/index.js
@@ -3,10 +3,12 @@
 /**
  * Internal dependencies
  */
-import { initHappychat } from 'src';
+import { initHappychat, subscribeTo, unsubscribeFrom } from 'src';
 
 window.Happychat = {
 	open: ( { nodeId, groups, accessToken } ) => {
 		initHappychat( { nodeId, groups, accessToken } );
 	},
+	on: ( eventName, subscriber ) => subscribeTo( eventName, subscriber ),
+	off: ( eventName, subscriber ) => unsubscribeFrom( eventName, subscriber ),
 };
