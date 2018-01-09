8e34c101b0ab72d6322dd75ac12dda5070771947
Andrés Maneiro <andres.maneiro@automattic.com>
2018-01-09 19:25:34 +0100

    Select Drodown item component: fix tests

--- src/ui/components/select-dropdown/test/item.js
+++ src/ui/components/select-dropdown/test/item.js
@@ -6,10 +6,8 @@
 /**
  * External dependencies
  */
-import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import React from 'react';
-import sinon from 'sinon';
 
 /**
  * Internal dependencies
@@ -20,33 +18,33 @@ describe( 'item', () => {
 	describe( 'component rendering', () => {
 		test( 'should render a list entry', () => {
 			const dropdownItem = shallow( <SelectDropdownItem>Published</SelectDropdownItem> );
-			expect( dropdownItem.is( 'li.select-dropdown__option' ) ).to.be.true;
+			expect( dropdownItem.is( 'li.select-dropdown__option' ) ).toBeTruthy();
 		} );
 
 		test( 'should contain a link', () => {
 			const dropdownItem = shallow( <SelectDropdownItem>Published</SelectDropdownItem> );
-			expect( dropdownItem.children( 'a.select-dropdown__item' ).length ).to.eql( 1 );
-			expect( dropdownItem.find( 'span.select-dropdown__item-text' ).text() ).to.eql( 'Published' );
+			expect( dropdownItem.children( 'a.select-dropdown__item' ).length ).toBe( 1 );
+			expect( dropdownItem.find( 'span.select-dropdown__item-text' ).text() ).toBe( 'Published' );
 		} );
 
 		test( 'should not have `tabindex` attribute, when the parent dropdown is closed', () => {
 			const dropdownItem = shallow(
 				<SelectDropdownItem isDropdownOpen={ false }>Published</SelectDropdownItem>
 			);
-			expect( dropdownItem.children( { tabIndex: 0 } ).length ).to.eql( 0 );
+			expect( dropdownItem.children( { tabIndex: 0 } ).length ).toBe( 0 );
 		} );
 
 		test( 'should have `tabindex` attribute set to `0`, only when the parent dropdown is open (issue#9206)', () => {
 			const dropdownItem = shallow(
 				<SelectDropdownItem isDropdownOpen={ true }>Published</SelectDropdownItem>
 			);
-			expect( dropdownItem.children( { tabIndex: 0 } ).length ).to.eql( 1 );
+			expect( dropdownItem.children( { tabIndex: 0 } ).length ).toBe( 1 );
 		} );
 	} );
 
 	describe( 'when the component is clicked', () => {
 		test( 'should do nothing when is disabled', () => {
-			const onClickSpy = sinon.spy();
+			const onClickSpy = jest.fn();
 			const dropdownItem = shallow(
 				<SelectDropdownItem disabled={ true } onClick={ onClickSpy }>
 					Published
@@ -54,19 +52,19 @@ describe( 'item', () => {
 			);
 
 			const link = dropdownItem.children( 'a.select-dropdown__item' );
-			expect( link.hasClass( 'is-disabled' ) ).to.be.true;
+			expect( link.hasClass( 'is-disabled' ) ).toBeTruthy();
 
 			link.simulate( 'click' );
-			sinon.assert.notCalled( onClickSpy );
+			expect( onClickSpy.mock.calls.length ).toBe( 0 );
 		} );
 
 		test( 'should run the `onClick` hook', () => {
-			const onClickSpy = sinon.spy();
+			const onClickSpy = jest.fn();
 			const dropdownItem = shallow(
 				<SelectDropdownItem onClick={ onClickSpy }>Published</SelectDropdownItem>
 			);
 			dropdownItem.children( 'a.select-dropdown__item' ).simulate( 'click' );
-			sinon.assert.calledOnce( onClickSpy );
+			expect( onClickSpy.mock.calls.length ).toBe( 1 );
 		} );
 	} );
 } );
