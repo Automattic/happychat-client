3db164530ccca80e2941f072ebd4e2f4d1e52121
Andrés Maneiro <andres.maneiro@automattic.com>
2018-01-09 17:13:09 +0100

    Disable form API until is ready

--- targets/wordpress/index.js
+++ targets/wordpress/index.js
@@ -5,9 +5,13 @@
  */
 import { initHappychat, subscribeTo, unsubscribeFrom } from 'src';
 
+// TODO enable form
+// form is not ready to be embedded, so hold off enabling the API in the meanwhile
 window.Happychat = {
-	open: ( { nodeId, groups, accessToken, howCanWeHelpOptions, howDoYouFeelOptions } ) => {
-		initHappychat( { nodeId, groups, accessToken, howCanWeHelpOptions, howDoYouFeelOptions } );
+	// open: ( { nodeId, groups, accessToken, howCanWeHelpOptions, howDoYouFeelOptions } ) => {
+	open: ( { nodeId, groups, accessToken } ) => {
+		// initHappychat( { nodeId, groups, accessToken, howCanWeHelpOptions, howDoYouFeelOptions } );
+		initHappychat( { nodeId, groups, accessToken } );
 	},
 	on: ( eventName, subscriber ) => subscribeTo( eventName, subscriber ),
 	off: ( eventName, subscriber ) => unsubscribeFrom( eventName, subscriber ),
