19a4158365558c4c1f8e6e509e409e9a027ac74e
Andrés Maneiro <andres.maneiro@automattic.com>
2018-01-10 10:59:59 +0100

    Tell jest how to process enzyme output

--- setup-jest.js
+++ setup-jest.js
@@ -7,3 +7,7 @@ import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-15';
 
 Enzyme.configure( { adapter: new Adapter() } );
+
+// configure snapshot serializer for enzyme
+const { createSerializer } = require.requireActual( 'enzyme-to-json' );
+expect.addSnapshotSerializer( createSerializer( { mode: 'deep' } ) );
