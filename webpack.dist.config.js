7aaccc6eaa5d1f2e6d280f2c618892126df838d1
Andrés Maneiro <andres.maneiro@automattic.com>
2018-01-12 13:16:28 +0100

    Add dist scripts

---
+++ webpack.dist.config.js
@@ -0,0 +1,31 @@
+/** @format */
+const path = require( 'path' );
+const webpack = require( 'webpack' );
+const UglifyJsPlugin = require( 'uglifyjs-webpack-plugin' );
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		filename: './dist/happychat.min.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				use: 'babel-loader',
+				include: [ path.resolve( __dirname, 'src' ), path.resolve( __dirname, 'targets' ) ],
+			},
+		],
+	},
+	plugins: [
+		new webpack.DefinePlugin( {
+			'process.env.NODE_ENV': JSON.stringify( 'production' ),
+		} ),
+		new webpack.optimize.ModuleConcatenationPlugin(),
+		new UglifyJsPlugin(),
+	],
+	resolve: {
+		extensions: [ '.js', '.jsx' ],
+		modules: [ path.resolve( __dirname ), path.resolve( __dirname, 'node_modules' ) ],
+	},
+};
