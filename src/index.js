783c69232c0a482f18963bf9d14d3befcbb5aab4
Andrés Maneiro <andres.maneiro@automattic.com>
2018-01-25 14:06:08 +0100

    Revome subscribers code: not needed atm

--- src/index.js
+++ src/index.js
@@ -8,8 +8,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore, compose } from 'redux';
 import { devToolsEnhancer } from 'redux-devtools-extension';
-import debugFactory from 'debug';
-const debug = debugFactory( 'happychat-client:api' );
 
 /**
  * Internal dependencies
@@ -20,18 +18,7 @@ import { MessageForm } from 'src/ui/components/message-form';
 import reducer from 'src/state/reducer';
 import { socketMiddleware } from 'src/state/middleware';
 import { setCurrentUser, setGroups, setLocale } from 'src/state/user/actions';
-import isChatBeingAssigned from 'src/state/selectors/is-chat-being-assigned';
-import isChatAssigned from 'src/state/selectors/is-chat-assigned';
-import isAvailable from 'src/state/selectors/is-available';
 import { hasTouch } from 'src/lib/touch-detect';
-
-/**
- * Subscribers object. Each key is an event exposed by the Happychat API.
- */
-const subscribers = {
-	availability: [],
-	ongoingConversation: [],
-};
 
 const store = createStore(
 	reducer,
@@ -168,69 +155,4 @@ export const initHappychat = ( {
 			renderHappychat( { nodeId, howCanWeHelpOptions, howDoYouFeelOptions, fallbackTicketPath } )
 		)
 		.catch( renderError( nodeId ) );
-};
-
-const callSubscribersIfAvailabilityChanged = ( oldAvailability, newAvailability ) => {
-	if ( oldAvailability !== newAvailability ) {
-		debug( 'availability changed form ', oldAvailability, ' to ', newAvailability );
-		subscribers.availability.forEach( subscriber => {
-			subscriber( newAvailability );
-		} );
-	}
-	return newAvailability;
-};
-
-const callSubscribersIfOngoingConversationChanged = ( oldStatus, newStatus ) => {
-	if ( oldStatus !== newStatus ) {
-		debug( 'ongoingConversation changed form ', oldStatus, ' to ', newStatus );
-		subscribers.ongoingConversation.forEach( subscriber => {
-			subscriber( newStatus );
-		} );
-	}
-	return newStatus;
-};
-
-let oldAvailability = false;
-let oldOngoingConversation = false;
-store.subscribe( () => {
-	// Some clients hide the chat form status if the availability is false,
-	// but they don't want to hide it if there is an ongoingConversation.
-	//
-	// When an operator has 1 spot left, we're going to call subscribers
-	// with availability=false and ongoingConversation=true.
-	// But because the availability event happens before the chatStatus is "assigned",
-	// we need to consider the "assigning" chatStatus as an ongoingConversation,
-	// to prevent the race condition.
-	oldOngoingConversation = callSubscribersIfOngoingConversationChanged(
-		oldOngoingConversation,
-		isChatBeingAssigned( store.getState() ) || isChatAssigned( store.getState() )
-	);
-	oldAvailability = callSubscribersIfAvailabilityChanged(
-		oldAvailability,
-		isAvailable( store.getState() )
-	);
-} );
-
-/**
- * Subscribe to Happychat events.
- *
- * @param  {String} eventName  Name of the Happychat event to subscribe.
- * @param  {Function} subscriber Callback function
- */
-export const subscribeTo = ( eventName, subscriber ) => {
-	subscribers.hasOwnProperty( eventName ) && subscribers[ eventName ].indexOf( subscriber ) === -1
-		? subscribers[ eventName ].push( subscriber )
-		: '';
-};
-
-/**
- * Subscribe from Happychat events.
- *
- * @param  {String} eventName  Name of the Happychat event to unsubscribe.
- * @param  {Function} subscriber Callback function
- */
-export const unsubscribeFrom = ( eventName, subscriber ) => {
-	subscribers.hasOwnProperty( eventName ) && subscribers[ eventName ].indexOf( subscriber ) > -1
-		? subscribers[ eventName ].splice( subscribers[ eventName ].indexOf( subscriber ), 1 )
-		: '';
 };
