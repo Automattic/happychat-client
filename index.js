7fe82b41a6b3c7b069638babc458ace8d1ca7e58
Andrés Maneiro <andres.maneiro@automattic.com>
2018-02-07 10:24:12 +0100

    Extract happychat library from WordPress target

--- index.js
+++ index.js
@@ -3,13 +3,24 @@
 /**
  * Internal dependencies
  */
-// UI components
-export { HappychatConnection } from './src/ui/components/connection';
-export { Composer } from './src/ui/components/composer';
-export { Notices } from './src/ui/components/notices';
-export { Timeline } from './src/ui/components/timeline';
-export { Title } from './src/ui/components/title';
+import { initHappychat } from 'src';
 
-// State components
-import reducer from './src/state/reducer';
-export { reducer };
+window.Happychat = {
+	open: ( {
+		nodeId,
+		groups,
+		accessToken,
+		howCanWeHelpOptions,
+		howDoYouFeelOptions,
+		fallbackTicketPath,
+	} ) => {
+		initHappychat( {
+			nodeId,
+			groups,
+			accessToken,
+			howCanWeHelpOptions,
+			howDoYouFeelOptions,
+			fallbackTicketPath,
+		} );
+	},
+};
