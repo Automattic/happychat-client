58f7c7a4fc8f1eb183e37848252e8a1deb018781
Andrés Maneiro <andres.maneiro@automattic.com>
2018-02-14 18:50:08 +0100

    Teach rollup to use babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,12 @@
 /** @format */
 
+/**
+ * External dependencies
+ */
+import babel from 'rollup-plugin-babel';
+
 export default {
 	input: './index.js',
 	output: { file: './dist/happychat.rollup.js', format: 'iife' },
+	plugins: [ babel( { exclude: 'node_modules/**' } ) ],
 };
