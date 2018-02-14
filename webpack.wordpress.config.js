3a640192b810c09d874066f3e9a992e6002a3453
Andrés <nosolosw@users.noreply.github.com>
2018-02-14 12:53:26 +0100

    Update/config (#110)
    * Make the WordPress plugin use the development config
    
    * Production config points to production HC server

--- webpack.wordpress.config.js
+++ webpack.wordpress.config.js
@@ -19,7 +19,7 @@ module.exports = {
 	},
 	plugins: [
 		new webpack.DefinePlugin( {
-			'process.env.NODE_ENV': JSON.stringify( 'production' ),
+			'process.env.NODE_ENV': JSON.stringify( 'development' ),
 		} ),
 		new webpack.optimize.ModuleConcatenationPlugin(),
 		new UglifyJsPlugin(),
