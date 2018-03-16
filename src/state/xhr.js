34a099c9372ead245a60fe2203e927e579a085cc
Andrés <andres.maneiro@automattic.com>
2018-03-16 17:38:10 +0100

    src/state

--- src/state/xhr.js
+++ src/state/xhr.js
@@ -1,27 +1,27 @@
 /** @format */
 
 /**
-*
-* Given a Redux action and a timeout, dispatch a request.
-*
-* The request can have three states, and will dispatch an action accordingly:
-*
-* - request was succesful: would dispatch the action.callback with true as argument
-* - request was unsucessful: would dispatch the action.callback with false as argument
-* - request timeout: would dispatch action.callbackTimeout
-*
-* @param  { Function } dispatch Redux dispatch function
-* @param  { Object } action A Redux action with props
-*                  	{
-*                  		path: URL path used to send the request,
-*                  		payload: contents to be sent,
-*                  		callback: a Redux action creator,
-*                  		callbackTimeout: a Redux action creator,
-*                  	}
-* @param  { Number } timeout How long (in milliseconds) has the server to respond
-* @return { Promise } Fulfilled (returns the response)
-*                     or rejected (returns an error message)
-*/
+ *
+ * Given a Redux action and a timeout, dispatch a request.
+ *
+ * The request can have three states, and will dispatch an action accordingly:
+ *
+ * - request was succesful: would dispatch the action.callback with true as argument
+ * - request was unsucessful: would dispatch the action.callback with false as argument
+ * - request timeout: would dispatch action.callbackTimeout
+ *
+ * @param  { Function } dispatch Redux dispatch function
+ * @param  { Object } action A Redux action with props
+ *                  	{
+ *                  		path: URL path used to send the request,
+ *                  		payload: contents to be sent,
+ *                  		callback: a Redux action creator,
+ *                  		callbackTimeout: a Redux action creator,
+ *                  	}
+ * @param  { Number } timeout How long (in milliseconds) has the server to respond
+ * @return { Promise } Fulfilled (returns the response)
+ *                     or rejected (returns an error message)
+ */
 const makeRequest = ( dispatch, action, timeout ) => {
 	const xhr = new XMLHttpRequest();
 	xhr.open( 'POST', action.path, true );
