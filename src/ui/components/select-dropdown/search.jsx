a961668be774fee71914f1b95ab9d487cd785d0d
Andrés <andres.maneiro@automattic.com>
2018-03-27 17:40:01 +0200

    Focus on open

--- src/ui/components/select-dropdown/search.jsx
+++ src/ui/components/select-dropdown/search.jsx
@@ -7,9 +7,29 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
 class Search extends PureComponent {
+	constructor( props ) {
+		super( props );
+		this.focus = this.focus.bind( this );
+	}
+
+	componentDidMount() {
+		this.focus();
+	}
+
+	focus() {
+		this.searchInputElement && this.searchInputElement.focus();
+	}
+
 	render() {
 		const { placeholder } = this.props;
-		return <input type="search" role="search" placeholder={ placeholder } />;
+		return (
+			<input
+				ref={ c => ( this.searchInputElement = c ) } // eslint-disable-line react/jsx-no-bind, max-len
+				type="search"
+				role="search"
+				placeholder={ placeholder }
+			/>
+		);
 	}
 }
 
