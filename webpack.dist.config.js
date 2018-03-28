e3a19305c4bb025ac6276df91422eafdd343b9a3
undemian <undemian@gmail.com>
2018-03-28 12:10:34 +0300

    Remove lodash-webpack-plugin

--- webpack.dist.config.js
+++ webpack.dist.config.js
@@ -2,7 +2,6 @@
 const path = require( 'path' );
 const webpack = require( 'webpack' );
 const UglifyJsPlugin = require( 'uglifyjs-webpack-plugin' );
-const LodashModuleReplacementPlugin = require( 'lodash-webpack-plugin' );
 
 module.exports = {
 	entry: './targets/dist/index.js',
@@ -19,10 +18,6 @@ module.exports = {
 		],
 	},
 	plugins: [
-		new LodashModuleReplacementPlugin( {
-			paths: true,
-			shorthands: true,
-		} ),
 		new webpack.DefinePlugin( {
 			'process.env.NODE_ENV': JSON.stringify( 'production' ),
 		} ),
