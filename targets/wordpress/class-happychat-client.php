44e7d09deb3d6246f8f5b0be7686ae06e393b59a
Dwain Maralack <dwain.maralack@gmail.com>
2018-04-30 16:01:15 +0200

    move entry validation back to the top

--- targets/wordpress/class-happychat-client.php
+++ targets/wordpress/class-happychat-client.php
@@ -96,13 +96,14 @@ class Happychat_Client {
 
 		$happychat_settings = apply_filters( 'happychat_settings', $happychat_settings );
 
+		$happychat_settings['entry']  = $this->validate_entry( $happychat_settings['entry'] );
+
 		$path_to_create = isset( $happychat_settings['entryOptions']['fallbackTicket']['pathToCreate'] ) ? $happychat_settings['entryOptions']['fallbackTicket']['pathToCreate'] : null;
 		$path_to_show   = isset( $happychat_settings['entryOptions']['fallbackTicket']['pathToShow'] ) ? $happychat_settings['entryOptions']['fallbackTicket']['pathToShow'] : null;
 
 		$happychat_settings['entryOptions']['fallbackTicket']['pathToCreate'] = $this->validate_path( $path_to_create );
 		$happychat_settings['entryOptions']['fallbackTicket']['pathToShow']   = $this->validate_path( $path_to_show );
 
-		$happychat_settings['entry']  = $this->validate_entry( $happychat_settings['entry'] );
 		$happychat_settings['groups'] = [ $this->validate_group( $happychat_settings['groups'][0] ) ];
 
 		return $happychat_settings;
