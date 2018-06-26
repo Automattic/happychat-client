edbcbdb1beb04eb31e72361197b4f82876ab6f95
Andrés <andres.maneiro@automattic.com>
2018-06-26 17:49:20 +0200

    lodash: import module not the full library

--- src/ui/components/form-description/index.jsx
+++ src/ui/components/form-description/index.jsx
@@ -6,7 +6,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import { omit } from 'lodash';
+import omit from 'lodash/omit';
 
 const createMarkup = contents => ( {
 	__html: contents,
