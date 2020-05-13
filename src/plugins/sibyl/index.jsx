d8ed75b7b20de18ffa8794c664699568f61541ff
Matt Wondra <matt.wondra@automattic.com>
2020-05-12 20:52:38 -0500

    Make styling a bit more compact, and clean up

--- src/plugins/sibyl/index.jsx
+++ src/plugins/sibyl/index.jsx
@@ -5,7 +5,7 @@
  */
 import React from 'react';
 // import PropTypes from 'prop-types';
-import { debounce } from 'lodash';
+import { debounce, isEmpty } from 'lodash';
 import wpcomRequest from 'wpcom-xhr-request';
 
 
@@ -17,24 +17,14 @@ import FormLabel from 'src/ui/components/form-label';
 
 export default class Sibyl extends React.Component {
 	state = {
-		isLoading: false,
 		suggestions: [],
 	};
 
-	receiveSuggestions = (suggestions) => {
-		this.setState( {
-			isLoading: false,
-			suggestions,
-		} );
-	}
-
 	fetchSuggestions = debounce(() => {
-		this.setState( { isLoading: true } );
-
 		const query = `${this.props.subject} ${this.props.message}`.trim();
 
 		if ( ! query ) {
-			this.receiveSuggestions( [] );
+			this.setState( { suggestions: [] } );
 			return;
 		}
 
@@ -46,8 +36,7 @@ export default class Sibyl extends React.Component {
 				query,
 			},
 		}, ( error, body, headers ) => {
-			// TODO: Error handling
-			this.receiveSuggestions( Array.isArray( body ) ? body : [] );
+			this.setState( { suggestions: Array.isArray( body ) ? body : [] } );
 		} );
 	}, 400)
 
@@ -62,7 +51,7 @@ export default class Sibyl extends React.Component {
 	}
 
 	render() {
-		if ( ! this.state.suggestions || this.state.suggestions.length === 0 ) {
+		if ( isEmpty( this.state.suggestions ) ) {
 			return null;
 		}
 
@@ -72,7 +61,15 @@ export default class Sibyl extends React.Component {
 					Do any of these Frequently Asked Questions help?
 				</Card>
 				{ this.state.suggestions.map( ( { id, link, title } ) => (
-					<Card key={id} className="sibyl--suggestion-card" compact={true} href={link} target="_blank">{title}</Card>
+					<Card
+						key={id}
+						className="sibyl--suggestion-card"
+						compact={true}
+						href={link}
+						target="_blank"
+					>
+						{title}
+					</Card>
 				) ) }
 			</div>
 		);
