8af392ac53671a58f8497d05dea4aecf0d99860c
Matt Wondra <matt.wondra@automattic.com>
2020-07-28 19:10:43 -0500

    Bump version to 0.0.7

--- targets/wordpress/plugin/class-happychat-client.php
+++ targets/wordpress/plugin/class-happychat-client.php
@@ -6,7 +6,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 class Happychat_Client {
 	private static $_instance = null;
-	const VERSION             = '0.0.5-dev';
+	const VERSION             = '0.0.7';
 
 	/**
 	* Create instance of class
