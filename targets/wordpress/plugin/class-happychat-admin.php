68e45238bf494211fcc6a07a2c9f2e6729942918
Matt Wondra <matt.wondra@automattic.com>
2021-04-26 23:01:25 -0500

    Bump version to clear widgets cache

--- targets/wordpress/plugin/class-happychat-admin.php
+++ targets/wordpress/plugin/class-happychat-admin.php
@@ -6,7 +6,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 class Happychat_Admin {
 	private static $_instance = null;
-	const VERSION             = '0.0.9';
+	const VERSION             = '0.0.10';
 
 	/**
 	 * Create instance of class
