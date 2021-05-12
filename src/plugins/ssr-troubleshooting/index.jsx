32926b729a703ced3f345610f2141fbd5e11707e
Matt Wondra <matt.wondra@automattic.com>
2021-05-11 19:45:03 -0500

    Updates to UI, and add feedback button

--- src/plugins/ssr-troubleshooting/index.jsx
+++ src/plugins/ssr-troubleshooting/index.jsx
@@ -8,7 +8,9 @@ import React from 'react';
 import { debounce, isEmpty } from 'lodash';
 import wpcomRequest from 'wpcom-xhr-request';
 import classnames from 'classnames';
+import GridiconCheckmarkCircle from 'gridicons/dist/checkmark-circle';
 import GridiconChevronRight from 'gridicons/dist/chevron-right';
+import GridiconThumbsUp from 'gridicons/dist/thumbs-up';
 
 /**
  * Internal dependencies
@@ -21,32 +23,42 @@ const TESTFLAGS = [
 	{
 		code: 'woocommerce_is_outdated',
 		title: "Your WooCommerce plugin is outdated.",
-		text: 'Updating WooCommerce ensures that you have the latest bug fixes and system compatibility.<a href="">Read more about backing up and updating your site.</a>'
+		text: "It’s important to keep WordPress, WooCommerce, and any plugins up to date to ensure that your website functions as expected. Often times, current issues can be resolved simply by updating your website's software.",
+		link_text: 'Read more about backing up and updating your site.',
+		link_href: '',
 	},
 	{
 		code: 'woocommerce_is_outdated',
 		title: "Your WooCommerce plugin is outdated.",
-		text: 'Updating WooCommerce ensures that you have the latest bug fixes and system compatibility.<a href="">Read more about backing up and updating your site.</a>'
+		text: "It’s important to keep WordPress, WooCommerce, and any plugins up to date to ensure that your website functions as expected. Often times, current issues can be resolved simply by updating your website's software.",
+		link_text: 'Read more about backing up and updating your site.',
+		link_href: '',
 	},
 	{
 		code: 'woocommerce_is_outdated',
 		title: "Your WooCommerce plugin is outdated.",
-		text: 'Updating WooCommerce ensures that you have the latest bug fixes and system compatibility.<a href="">Read more about backing up and updating your site.</a>'
+		text: "It’s important to keep WordPress, WooCommerce, and any plugins up to date to ensure that your website functions as expected. Often times, current issues can be resolved simply by updating your website's software.",
+		link_text: 'Read more about backing up and updating your site.',
+		link_href: '',
 	},
 ];
 
 export default class SSRTroubleshooting extends React.Component {
 	state = {
-		flags: [],
+		flags: TESTFLAGS,
 		flagClicked: false,
 		expandedFlags: [],
+		feedbackGivenOn: [],
 	};
 
 	fetchFlags = debounce(() => {
 		const { ssr } = this.props;
 
+		const uiStateDefaults = { expandedFlags: [], feedbackGivenOn: [] };
+
+		// Reset some of the UI state
 		if ( ! ssr ) {
-			this.setState( { flags: [] } );
+			this.setState( { flags: [], ...uiStateDefaults } );
 			return;
 		}
 
@@ -64,28 +76,11 @@ export default class SSRTroubleshooting extends React.Component {
 				return;
 			}
 
-			this.setState( { flags: body } );
+			this.setState( { flags: body, ...uiStateDefaults } );
 		} );
 	}, 400)
 
 	handleFormSubmit = () => {
-		// if ( this.state.suggestionClicked ) {
-		// 	recordEvent( 'happychatclient_sibyl_support_after_question_click', { site } );
-		// }
-
-		// if ( isEmpty( this.state.suggestions ) ) {
-		// 	recordEvent( 'happychatclient_sibyl_support_without_matching_questions', {
-		// 		site,
-		// 		query: this.getSearchQuery(),
-		// 	} );
-		// } else {
-		// 	recordEvent( 'happychatclient_sibyl_support_with_questions_showing', {
-		// 		site,
-		// 		query: this.getSearchQuery(),
-		// 		suggestions: this.state.suggestions.map( ({id, title}) => `${id} - ${title}` ).join(' / '),
-		// 	} );
-		// }
-		
 		const { ssr } = this.props;
 		const { flags } = this.state;
 
@@ -101,11 +96,32 @@ export default class SSRTroubleshooting extends React.Component {
 				} );
 			} );
 		}
+
+		// TODO: Also track other submission data, like these from Sibyl --->
+		//
+		// if ( this.state.suggestionClicked ) {
+		// 	recordEvent( 'happychatclient_sibyl_support_after_question_click', { site } );
+		// }
+		//
+		// if ( isEmpty( this.state.suggestions ) ) {
+		// 	recordEvent( 'happychatclient_sibyl_support_without_matching_questions', {
+		// 		site,
+		// 		query: this.getSearchQuery(),
+		// 	} );
+		// } else {
+		// 	recordEvent( 'happychatclient_sibyl_support_with_questions_showing', {
+		// 		site,
+		// 		query: this.getSearchQuery(),
+		// 		suggestions: this.state.suggestions.map( ({id, title}) => `${id} - ${title}` ).join(' / '),
+		// 	} );
+		// }
 	}
 
 	componentDidMount() {
 		this.props.addFormSubmitListener( this.handleFormSubmit );
-		this.fetchFlags();
+		if ( this.props.ssr ) {
+			this.fetchFlags();
+		}
 	}
 
 	componentDidUpdate( prevProps, prevState ) {
@@ -143,10 +159,16 @@ export default class SSRTroubleshooting extends React.Component {
 		// } );
 	}
 
-	render() {
-		// TODO Only show if this is an "SSR" component?
+	handleFeedbackClick( flagIndex ) {
+		const { feedbackGivenOn } = this.state;
+		this.setState( { feedbackGivenOn: [ ...feedbackGivenOn, flagIndex ] } );
+		return false;
+	}
 
-		if ( false && isEmpty( this.state.flags ) ) {
+	render() {
+		const { feedbackGivenOn, flags } = this.state;
+
+		if ( isEmpty( this.state.flags ) ) {
 			return null;
 		}
 
@@ -162,7 +184,7 @@ export default class SSRTroubleshooting extends React.Component {
 					help us support you better:
 				</p>
 				<Card className="ssr-troubleshooting__flags">
-					{ this.state.flags.map( ({code, title, text}, idx) => {
+					{ flags.map( ({code, title, text, link_text, link_href}, idx) => {
 						const classes = classnames( 'ssr-troubleshooting__flag', {
 							'ssr-troubleshooting__flag--is-expanded': this.state.expandedFlags.indexOf(idx) !== -1,
 						} );
@@ -176,7 +198,28 @@ export default class SSRTroubleshooting extends React.Component {
 									</span>
 								</button>
 								<div className="ssr-troubleshooting__flag-body">
-									<div dangerouslySetInnerHTML={{__html: text}} />
+									<p>{text}</p>
+									<a
+										className="button ssr-troubleshooting__flag-link"
+										target="_blank"
+										rel="noopener noreferrer"
+										href={link_href}
+									>
+										{link_text}
+									</a>
+									<a
+										className="ssr-troubleshooting__feedback-button"
+										href="#"
+										onClick={evt => {
+											evt.preventDefault();
+											this.handleFeedbackClick(idx);
+										} }
+									>
+										{ feedbackGivenOn.indexOf(idx) === -1
+											? <span><GridiconThumbsUp size={16} /> This helped me </span>
+											: <span><GridiconCheckmarkCircle size={16} /> Thanks for the feedback!</span>
+										}
+									</a>
 								</div>
 							</div>
 						);
