009f9560ec6ac5d844bffb8cc6bf83619a77dfb0
Matt Wondra <matt.wondra@automattic.com>
2021-06-04 00:19:36 -0500

    Update Tracks lib to work in test environment

--- src/lib/tracks/index.js
+++ src/lib/tracks/index.js
@@ -1,12 +1,19 @@
 /** @format */
 
+let enabled = ( typeof document !== 'undefined' && typeof window !== 'undefined' );
+
 // Asynchronously load the Tracks script if this module is used
-const script = document.createElement( 'script' );
-script.src = '//stats.wp.com/w.js';
-document.head.appendChild( script );
-window._tkq = window._tkq || [];
+if ( enabled ) {
+	const script = document.createElement( 'script' );
+	script.src = '//stats.wp.com/w.js';
+	document.head.appendChild( script );
+	window._tkq = window._tkq || [];
+}
 
 export const recordEvent = ( name, properties = {} ) => {
+	if ( ! enabled ) {
+		return;
+	}
 	const propertiesWithDefaults = {
 		...properties,
 		happychatclient_version: require('/package.json').version,
